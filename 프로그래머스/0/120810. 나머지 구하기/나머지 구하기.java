c708ee5ccd0c433c6839a0cd7855dd0b9b6e9315
coding-suk <141230562+coding-suk@users.noreply.github.com>
2024-12-06 16:16:07 +0900

    [level 0] Title: 나머지 구하기, Time: 0.02 ms, Memory: 88 MB -BaekjoonHub

---
+++ 프로그래머스/0/120810. 나머지 구하기/나머지 구하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        // int answer = -1;
+        int answer;
+        
+        answer = num1 % num2;
+        
+        return answer;
+    }
+}
